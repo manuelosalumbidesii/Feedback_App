4545d6fa5ce60e9d72c79d8676f6fb4d03eba95e
Manuelo Salumbides II <manuelosalumbidesii@gmail.com>
2025-07-10 14:31:41 +0800

    update

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,14 +1,15 @@
 from fastapi.testclient import TestClient
-from backend.main import app  # or just `from main import app` if same directory
+from backend.main import app
+
 
 client = TestClient(app)
+
 
 def test_home_status_code():
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_home_content():
     response = client.get("/")
-    data = response.json()
-    assert data["status"] == "OK"
-    assert data["message"] == "Feedback API is running"
+    assert "Feedback API is running" in response.text
